a4224d528649e66c4080ff9c78f4f051157312d4
Justin <justin.yip@redalphacyber.com>
2025-11-13 16:31:08 +0800

    Forgot braces on lower

--- capstone/solutionevaluator.py
+++ capstone/solutionevaluator.py
@@ -442,7 +442,7 @@ def get_llm_setting (llm_choice: str):
         return LLM_SETTINGS.keys()
     
     # Otherwise return 
-    return LLM_SETTINGS[llm_choice.lower]
+    return LLM_SETTINGS[llm_choice.lower()]
 
 def llm_eval_student_batch_process (student_submissions_data : list[str], questions_answers : list[dict]):
     """
